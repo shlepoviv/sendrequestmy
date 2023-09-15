8e9b758ede82d13be3041eb5c19c511d5793b4e7
Slepov Ivan <Slepov-Ivan@mail.ru>
2023-09-15 19:22:51 +0300

    1st lesson

---
+++ src/enums/global_enums.py
@@ -0,0 +1,5 @@
+from enum import Enum
+
+class GLOBAL_ERORR_MASSEGE(Enum):
+    WRONG_COdE= 'Wrong respose code'
+    WRONG_ENUM_CONT = 'Count of enum difference of expected'
---
+++ tests/some_test.py
@@ -0,0 +1,7 @@
+import requests
+
+def test_getting_post():
+    assert True , 'test test'
+
+def test_some2():
+    assert False, 'fail test'
