266fbe73d8c2f38a2ab2d80e1dda9c894333cf6f
Slepov Ivan <Slepov-Ivan@mail.ru>
2023-09-16 12:06:55 +0300

    add self descripshen in Respone class

--- src/base_clases/response.py
+++ src/base_clases/response.py
@@ -1,5 +1,3 @@
-from src.enums.global_enums import GLOBAL_ERORR_MASSEGE
-
 class Response:
     def __init__(self,response) -> None:
         self.response = response
@@ -8,9 +6,9 @@ class Response:
 
     def valisete_status_code(self,status_code):
         if isinstance(status_code,list):
-            assert self.status_code in status_code , GLOBAL_ERORR_MASSEGE.WRONG_COdE.value
+            assert self.status_code in status_code , self
         else:
-            assert self.status_code == status_code
+            assert self.status_code == status_code , self
 
     def validete_data(self,schema):
         if isinstance(self.response_data,list):
@@ -18,6 +16,11 @@ class Response:
                 schema.model_validate(c)
         else:
             schema.model_validate(self.response_data)
+    
+    def __str__(self) -> str:
+        return f'\n'\
+            f'Status code= {self.status_code} \n'\
+            f'Response data= {self.response_data}'
 
     
     
--- tests/some_test.py
+++ tests/some_test.py
@@ -8,7 +8,7 @@ from src.pydantic_schemas.base_schem import Company
 def test_getting_post():
     r = requests.get(url=TESTED_URL,timeout=10)
     respose = Response(r)
-    respose.valisete_status_code(200)
+    respose.valisete_status_code(300)
     respose.validete_data(Company)
 
 if __name__ == '__main__':
