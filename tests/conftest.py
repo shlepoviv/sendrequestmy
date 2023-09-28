d6fe3ed5c7b312241a13c2a8158c4a587e210a20
Slepov Ivan <Slepov-Ivan@mail.ru>
2023-09-28 18:44:45 +0300

    add post fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -18,9 +18,29 @@ def _get_post(api_path,object_id = None,params_req=None,headers = None):
     r = requests.get(url=resurl,params=params_req,headers=headers,timeout=10)
     return r
 
-# companies/
+
 @pytest.fixture()
 def get_post():
     return _get_post
 
-_get_post('companies',object_id = 'f',headers ={'Accept-Language': 'Pu'})
+
+def _post_post(api_path,object_id = None,params_req=None,headers = None):
+    resurl=TESTED_URL
+    if not isinstance(api_path,list):
+        api_path = [api_path]
+    for lvl in api_path:
+        resurl = resurl + f'{lvl}/' 
+    if  object_id:      
+        resurl = resurl + f'{object_id}' 
+    if not params_req:
+        params_req = {'None':None}
+    if not headers:
+        headers = {'None':None}
+        
+    r = requests.post(url=resurl,params=params_req,headers=headers,timeout=10)
+    return r
+
+@pytest.fixture()
+def post_post():
+    return _post_post
+
