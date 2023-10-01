e7529ce238e645a0d74dae2b7fc2d7097e346357
Slepov Ivan <Slepov-Ivan@mail.ru>
2023-10-01 13:31:27 +0300

    add 2 test authorization with token

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,11 +1,15 @@
+from time import sleep
 import pytest
 
 from src.base_clases.param_requests import ApiRequests
 from src.base_clases.response import Response
 
 from src.generators.user_generetor import UserGenerator
+from src.generators.login_password_generator import LoginPasswordGenerator
 
 from src.enums.company_enums import CompanyStatys
+
+from configure import PASSWORD_AUTH
 
 @pytest.fixture()
 def request_company():
@@ -50,3 +54,28 @@ def closed_company():
 def request_auth():
     req = ApiRequests(api_path=['auth','authorize'])
     return req
+
+@pytest.fixture()
+def request_auth_with_token():
+    req = ApiRequests(api_path=['auth'],object_id ='me')
+    return req
+
+@pytest.fixture()
+def valide_token():
+    req = ApiRequests(api_path=['auth','authorize'])
+    data = LoginPasswordGenerator().get_object()
+    data['password'] = PASSWORD_AUTH
+    req.set_parameters(data = data)
+    respose = Response(req.send_post())
+    return respose.response_data['token']
+
+@pytest.fixture()
+def expired_token():
+    req = ApiRequests(api_path=['auth','authorize'])
+    data = LoginPasswordGenerator().get_object()
+    data['password'] = PASSWORD_AUTH
+    data['timeout'] = 1
+    req.set_parameters(data = data)
+    respose = Response(req.send_post())
+    sleep(1)
+    return respose.response_data['token']
--- tests/test_auth.py
+++ tests/test_auth.py
@@ -1,7 +1,7 @@
 import pytest
 
 from src.pydantic_schemas.service_data import HTTPValidationError
-from src.pydantic_schemas.user import TokenResponse
+from src.pydantic_schemas.user import TokenResponse, MeResponse
 
 from src.base_clases.response import Response
 
@@ -47,9 +47,48 @@ def test_unsuccessful_auth(request_auth,param,val):
         data[param] = val
     else:
         if not data.get(param,'non_expec') == 'non_expec': data.pop(param) 
-    print(data)
     request_auth.set_parameters(data = data)
     respose = Response(request_auth.send_post())
     respose.valisete_status_code([403, 404, 422])
     if respose.status_code == 422:
         respose.validete_data(HTTPValidationError)
+
+def test_successful_with_token(request_auth_with_token,valide_token):
+    '''
+    Check successful authorization with token
+    Проверка успешной авторизации с токеном
+    '''
+    header = {'x-token':valide_token}
+    request_auth_with_token.set_parameters(headers = header)
+    respose = Response(request_auth_with_token.send_get())
+    respose.valisete_status_code(200)
+    respose.validete_data(MeResponse)
+
+
+@pytest.mark.parametrize('param,val',
+                         [('',None),
+                          ('x-token',None),
+                          ('x-token','qwerty')])
+def test_unsuccessful_with_token(request_auth_with_token,expired_token,param,val):
+    '''
+    Check unsuccessful authorization with token
+    Проверка неудачной авторизации с токеном
+    '''
+    header = {'x-token':expired_token}
+    if val:
+        header[param] = val
+    else:
+        if not header.get(param,'non_expec') == 'non_expec': header.pop(param) 
+    request_auth_with_token.set_parameters(headers = header)
+    respose = Response(request_auth_with_token.send_get())
+    respose.valisete_status_code([401,403,404, 422])
+    if respose.status_code == 422:
+        respose.validete_data(HTTPValidationError)
+
+
+
+
+
+# Попытка получение данных без токена.
+# Попытка получения данных с невалидным токеном.
+# Попытка получения данных с просроченным токеном.
